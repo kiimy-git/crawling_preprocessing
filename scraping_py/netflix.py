1d82831dbf87394d6ed249180b9536f62d5006a7
YOUNG HUN KIM <83389640+kimmy-git@users.noreply.github.com>
2023-02-13 13:12:46 +0900

    Update netflix.py

--- scraping_py/netflix.py
+++ scraping_py/netflix.py
@@ -1,69 +1,3 @@
-'''
-## search 아이콘 click
-//*[@id="appMountPoint"]/div/div/div[1]/div[1]/div[1]/div/div/div/div[1]/div/button/svg
-
-## searchbox text 입력
-element = driver.find_element("input#searchboxinput")
-element.send_keys(f"{drama_name}")
-
-----------로딩 짧게----------
-## Text입력 후 따로 button 누를 필요없음
-----------로딩 짧게----------
-
-## 첫번째 그림 click(= 찾는 드라마)
-//*[@id="title-card-0-0"]/div[1]/a/div[1]/img
-
-----------로딩 짧음----------
-##################################################################
-## <if 더보기 버튼 있으면 // 없을 수도 있음>
-//*[@id="appMountPoint"]/div/div/div[1]/div[2]/div/div[3]/div/div[2]/div/div/div[2]/div[11]/button
-
-## <에피소드 개수 찾기>
-#appMountPoint > div > div > div:nth-child(1) > div.focus-trap-wrapper.previewModal--wrapper.detail-modal.has-smaller-buttons > div > div.previewModal--info > div > div:nth-child(2) > div > div > div.episodeSelector-container > div
-##################################################################
-
-## 영상 재생 click
-//*[@id="appMountPoint"]/div/div/div[1]/div[2]/div/div[1]/div[4]/div
-
-----------로딩----------
-cnt = 0
-while True:
-
-    ## 여기서 class 세 가지 = for문
-        ## 영상 설정 아이콘 click
-        //*[@id="appMountPoint"]/div/div/div[1]/div[1]/div[1]/div[6]/svg/path
-
-        ## 번역 설정 title="영어 [사람 번역]"
-        # title class = ["영어 [사람 번역]", "일본어 [사람 번역]", "중국어(간체) [사람 번역]"]
-        //*[@id="select2-destLanguage-container"]
-
-        ## 닫기 click
-        //*[@id="lln-options-modal"]/div/div[4]/div
-
-        ----------로딩----------
-
-        ## 내보내기 아이콘 click
-        //*[@id="appMountPoint"]/div/div/div[1]/div[1]/div[1]/div[5]/svg
-
-        ## Excel click
-        //*[@id="lln-export-modal"]/div/div[3]/div[1]/div[1]/span[2]/div[2]
-
-        ## 내보내기 click
-        //*[@id="llnExportModalExportBtn"]
-
-        ----------다운로드 로딩----------
-
-    cnt += 1
-    ## if 다음화 아이콘 있으면 click:
-        //*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[2]/div[3]/div/div/div[3]/div/div[4]/div[2]/button/div/svg
-    
-    ## else:
-        break
-    ----------로딩----------
-    
-
-'''
-
 import os
 import urllib3
 import urllib.request
@@ -103,15 +37,15 @@ def drama_script(drama_name):
 ## Netflix 링크
 driver.get("https://www.netflix.com/browse")
 
-# ## 자동 로그인
-# driver.find_element(By.NAME, 'userLoginId').send_keys("smilegate_ai@smilegate.com")
-# driver.find_element(By.NAME, 'password').send_keys("!SmilegateAI83#")
-# driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/form/button').click() # 로그인 클릭
-# time.sleep(3)
+## 자동 로그인
+driver.find_element(By.NAME, 'userLoginId').send_keys("smilegate_ai@smilegate.com")
+driver.find_element(By.NAME, 'password').send_keys("!SmilegateAI83#")
+driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/form/button').click() # 로그인 클릭
+time.sleep(3)
 
-# ## 프로필 선택
-# driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[1]/div[2]/div/div/ul/li[2]/div/a/div/div').click()
-# time.sleep(5)
+## 프로필 선택
+driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[1]/div[2]/div/div/ul/li[2]/div/a/div/div').click()
+time.sleep(5)
 
 ## 시리즈 선택
 driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[1]/div[1]/div/div[1]/ul/li[3]/a').click()
@@ -139,10 +73,13 @@ while cnt < len(elements):
         time.sleep(1)
         driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[2]/div/div[1]/div[4]/div').click()
         ## 스크립트 내보내기
+        # ...
+        break
         
     ## 더보기 클릭
     driver.find_element(By.XPATH, '//*[@id="row-7"]/div/div/div/span[2]').click()
     cnt += 1
+    
     
 
 ## 돋보기 아이콘 클릭
