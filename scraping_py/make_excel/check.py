73391a92da1759790cdf60b7971d038727fbf749
YOUNG HUN KIM <83389640+kimmy-git@users.noreply.github.com>
2023-02-24 17:58:01 +0900

    Add files via upload

--- scraping_py/make_excel/check.py
+++ scraping_py/make_excel/check.py
@@ -1,81 +1,80 @@
 import pandas as pd
 import os
-
-## 작업 폴더 변경
-os.chdir("C:\\Users\\yhunkim\\Desktop\\검수")
-try:    
-    if not os.path.exists(f"Check"):
-        os.makedirs("Check")
+from glob import glob
         
-    
-    if not os.path.exists(f"Check\\tts_check"):
-        os.makedirs("Check\\tts_check")
-        
-    if not os.path.exists(f"Check\\emo_check"):
-        os.makedirs("Check\\emo_check")
-        
-except OSError:
-    print("Error: 생성 실패")
-    
-folder_list = os.listdir()[1:]
-print(folder_list)
-
-def extract_case(df, folder, conditions):
-    # 밖에서 try구문 진행
-    ## except empty
-    if conditions == "not_tts":
-        return df[conditions][["문장 번호", "파일명", "TTS", "전사 문장"]]
-    
-    else:
-        return df[conditions][["문장 번호", "파일명", "TTS", "검수", "전사 문장"]]
-    
-    
 ## TTS = not None
-def tts_ch(folder):
-    
-    file_list = os.listdir(folder)
+def tts_ch(work_path, folder):
 
-
-    tts_df = []
-    for i in file_list:
-        ## 파일 이름에 ~$ 존재
-        i = i.replace("~$", "")
-        file_name = i.split("_")[0]
-        df = pd.read_excel(f"{folder}\\{i}")
-
-    
-        not_tts = df["TTS"].notnull()
-        if df[not_tts].empty:
-            continue
-        else:
-            try:
-                
-                tts_df.append(df[not_tts][["문장 번호", "파일명", "TTS", "전사 문장"]])
-                            
-            except:
-
-                tts_df.append(df[not_tts][["문장 번호", "파일명", "TTS", "정제 문장"]])
-        
-        
-    print(f"-----------------------tts_ch,{folder}-----------------------")
-    try:
+    ## 작업 폴더 변경
+    os.chdir(rf"{work_path}")
+    try:    
+        if not os.path.exists(f"Check"):
+            os.makedirs("Check")
             
-        df = pd.concat(tts_df)
-        writer = pd.ExcelWriter(f"Check\\tts_check\\{folder}_{len(df)}.xlsx", engine='xlsxwriter')
-        df.to_excel(writer, sheet_name= 'TTS_Check', index=False)
         
-        ws = writer.sheets["TTS_Check"]
-        ws.set_column('B:B', 24)
-        ws.set_column('C:C', 29)
-        ws.set_column('D:D', 60)
+        if not os.path.exists(f"Check\\tts_check"):
+            os.makedirs("Check\\tts_check")
+            
+        if not os.path.exists(f"Check\\emo_check"):
+            os.makedirs("Check\\emo_check")
+            
+            
+    except OSError:
+        print("Error: 생성 실패")
+
+    ## 해당 폴더의 폴더 리스트
+    os.chdir(folder)
+    folder_list = os.listdir(folder)
+    
+    for folder in folder_list:
+        if folder == "Check":
+            continue
         
+        os.chdir(folder)
+        file_list = glob("*.xlsx")
+    
+
+        tts_df = []
+        for i in file_list:
+            file_name = i.split("_")[0]
+            df = pd.read_excel(i)
+
         
-        print(pd.concat(tts_df))
-        
-        writer.close()
-    except:
-        print("다르게 말한 스크립트 없음")
-    print()
+            not_tts = (df["검수"].notnull()) & (df["검수"] != "o")
+            if df[not_tts].empty:
+                continue
+            else:
+                try:
+                    
+                    tts_df.append(df[not_tts][["문장 번호", "파일명", "검수", "틀린부분", "전사 문장"]])
+                                
+                except:
+
+                    tts_df.append(df[not_tts][["문장 번호", "파일명", "검수", "틀린부분", "정제 문장"]])
+            
+            
+        print(f"-----------------------tts_ch,{folder}-----------------------")
+        os.chdir(rf"{work_path}")
+        try:
+            if tts_df:
+                df = pd.concat(tts_df)
+                writer = pd.ExcelWriter(f"Check\\tts_check\\{folder}_{len(df)}.xlsx", engine='xlsxwriter')
+                df.to_excel(writer, sheet_name= 'TTS_Check', index=False)
+                
+                ws = writer.sheets["TTS_Check"]
+                ws.set_column('B:B', 24)
+                ws.set_column('C:C', 29)
+                ws.set_column('D:D', 60)
+                ws.set_column('E:E', 60)
+                
+
+                
+                writer.close()
+            else:
+                print(f"{folder} -- 잘못된 스크립트 없음")
+        except Exception as e:
+            print(e)
+        print()
 
 ## 검수 = not "O" 
 ## 중복 코드 변경
@@ -88,15 +87,15 @@ def emo_ch(folder):
         file_name = i.split("_")[0]
         df = pd.read_excel(f"{folder}\\{i}")
 
-        not_collect = (df["검수"].notnull()) & (df["검수"] != "O")
+        not_collect = (df["검수"].notnull()) & (df["검수"] != "o")
         if df[not_collect].empty:
             continue
         else:
             try:
-                emo_df.append(df[not_collect][["문장 번호", "파일명", "TTS", "검수", "전사 문장"]])
+                emo_df.append(df[not_collect][["문장 번호", "파일명", "틀린부분", "검수", "전사 문장"]])
                     
             except:
-                emo_df.append(df[not_collect][["문장 번호", "파일명", "TTS", "검수", "정제 문장"]])
+                emo_df.append(df[not_collect][["문장 번호", "파일명", "틀린부분", "검수", "정제 문장"]])
         
     # print(emo_df)
     print(f"-----------------------emo_ch, {folder}-----------------------")
@@ -117,29 +116,3 @@ def emo_ch(folder):
     except:
         print("다르게 표현한 스크립트 없음")
     print()   
-
-import shutil
-for folder in folder_list[1:5]:
-
-    tts_ch(folder)
-    emo_ch(folder)
-
-    shutil.make_archive("check", "zip", "check")
-        
-
-## TTS = not None
-## 검수 = not "O"
-
-
-# file_list = os.listdir(folder_list[2])
-# emo_df = []
-
-# for i in file_list:
-#     df = pd.read_excel(f"{folder_list[2]}\\{i}")
-
-#     # if df[df["TTS"].notnull()][["파일명", "TTS", "검수", "전사 문장"]]
-#     # print(i, len(df[ (df["검수"].notnull()) & (df["검수"] != "O") ]))
-#     # print(len(df[ (df["검수"].notnull()) & (df["검수"] != "O") ]))
-#     emo_df.append(df[ (df["검수"].notnull()) & (df["검수"] != "O") ][["문장 번호", "파일명", "TTS", "검수", "전사 문장"]])
-# print(pd.concat(emo_df))
-
