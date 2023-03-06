a409a5035353eb35a60e3c7ba0decaa52d3d90ba
YOUNG HUN KIM <83389640+kimmy-git@users.noreply.github.com>
2023-03-06 13:10:11 +0900

    Add files via upload

--- scraping_py/test.py
+++ scraping_py/test.py
@@ -11,46 +11,7 @@ from tqdm import tqdm
 from multiprocessing import Pool
 import os
 
-def next_episode(driver):
-    
-    ## 다운로드 후 창이 한번 닫혔다가 다시 나옴(= 이 부분이 에러 요소임)
-    current_url = driver.current_url
-    driver.get(current_url)
-    time.sleep(7)
-
-    ## 무한 로딩시(=다음화 클릭시 ) 현재 창 다시 가져와서 클릭 버튼
-    try:
-        ## 마우스 위치 설정(= 다음 화 아이콘 있는 바 활성화(= .location))
-        action = webdriver.ActionChains(driver=driver)
-        position = driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div')
-        action.move_to_element(position).perform()
-        time.sleep(2)
-
-        next_icon = driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[2]/div[2]/div/div/div[3]/div/div[3]/div[2]/button')
-        next_icon.click()
-        time.sleep(10)
-        
-    except:
-        print("-----------------새로고침-----------------")
-        ## 새로고침
-        current_url = driver.current_url
-        driver.get(current_url)
-        time.sleep(5)
-        
-        ## 마우스 위치 설정(= 다음 화 아이콘 있는 바 활성화(= .location))
-        action = webdriver.ActionChains(driver=driver)
-        position = driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div')
-        action.move_to_element(position).perform()
-        time.sleep(2)
-
-        next_icon = driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[2]/div[2]/div/div/div[3]/div/div[3]/div[2]/button')
-        next_icon.click()
-        time.sleep(10)
-
-
-## 자막 언어 고정(= 번역언어만 변경)
-def trans_lang(first_video_path, total_episode):
-    
+def request_netflix(first_video_path):
     options = webdriver.ChromeOptions()
     # prefs = {'download.default_directory' : r'C:\Users\yhunkim\Desktop\netflix_translation6\ko-en\나홀로그대'}
     # options.add_experimental_option('prefs', prefs)
@@ -73,6 +34,77 @@ def trans_lang(first_video_path, total_episode):
     ## 드라마
     driver.get(first_video_path)
     time.sleep(10)
+    
+    return driver
+
+def skip_next_btn(driver):
+    ## ---------------- btn Xpath가 다를 수 있음 ---------------- ##
+    skip_btn = '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[2]/div[2]/button'
+    next_btn = '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[3]/div[2]/div/div/div[3]/div/div[3]/div[2]/button'
+    ## 영어, 일어
+    # //*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[3]/div[2]/div/div/div[3]/div/div[3]/div[2]/button
+    ## 중국어
+    # //*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[2]/div[2]/div/div/div[3]/div/div[3]/div[2]/button'
+
+    try:
+        ## opening skip ##
+        driver.find_element(By.XPATH, skip_btn).click()
+        time.sleep(2)
+
+        ## next btn ##
+        next_icon = driver.find_element(By.XPATH, next_btn)
+        next_icon.click()
+        time.sleep(10)
+    
+    ## skip btn이 없으면 다음화 클릭해서 진행
+    except:
+        next_icon = driver.find_element(By.XPATH, next_btn)
+        next_icon.click()
+        time.sleep(10)
+
+def next_episode(driver):
+    
+    ## 다운로드 후 창이 한번 닫혔다가 다시 나옴(= 이 부분이 에러 요소임)
+    current_url = driver.current_url
+    driver.get(current_url)
+    time.sleep(7)
+    
+    act_bar = '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div'
+
+    ## 무한 로딩시(=다음화 클릭시) 현재 창 다시 가져와서 클릭 버튼
+    try:
+        
+        ## 마우스 위치 설정(= 다음 화 아이콘 있는 바 활성화(= .location))
+        action = webdriver.ActionChains(driver=driver)
+        position = driver.find_element(By.XPATH, act_bar)
+        action.move_to_element(position).perform()
+        time.sleep(2)
+
+        ## opening skip 및 다음화 클릭 ##
+        skip_next_btn(driver=driver)
+        
+    except:
+        print("-----------------새로고침-----------------")
+        ## 새로고침
+        current_url = driver.current_url
+        driver.get(current_url)
+        time.sleep(5)
+        
+        ## 마우스 위치 설정(= 다음 화 아이콘 있는 바 활성화(= .location))
+        action = webdriver.ActionChains(driver=driver)
+        position = driver.find_element(By.XPATH, act_bar)
+        action.move_to_element(position).perform()
+        time.sleep(2)
+        
+        ## opening skip ##
+        skip_next_btn(driver=driver)
+
+
+## 자막 언어 고정(= 번역언어만 변경)
+def trans_lang(first_video_path, total_episode):
+    
+    ## Netflix
+    driver = request_netflix(first_video_path)
 
     ## 총 에피소드
     # while ep_cnt < total_episode:
@@ -104,29 +136,8 @@ def trans_lang(first_video_path, total_episode):
 ## 자막 언어 한국어로 변경(= 번역 언어만 변경)
 def change_cc(first_video_path, total_episode):
     
-    options = webdriver.ChromeOptions()
-    # prefs = {'download.default_directory' : r'C:\Users\yhunkim\Desktop\netflix_translation6\ko-en\나홀로그대'}
-    # options.add_experimental_option('prefs', prefs)
-    
-    ## GPU 가속 활성
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-setuid-sandbox')
-    options.add_argument('--no-sandbox')
-    
-    options.add_experimental_option('debuggerAddress', "127.0.0.1:9223")
-    ## extentions 적용 => 안됨 why?? / => 새로 extentions 설치로 진행
-    # options.add_argument("--load-extension={}".format(r'C:\Users\yhunkim\AppData\Local\Google\Chrome\User Data\Default\Extensions\hoombieeljmmljlkjmnheibnpciblicm\5.0.0_0'))
-    # options.add_extension(r"hoombieeljmmljlkjmnheibnpciblicm.crx")
-    driver = webdriver.Chrome(executable_path= r"C:\Users\yhunkim\Desktop\capture\chromedriver_win32 (1)\chromedriver.exe", options=options)
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-Agent', 'Mozila/5.0')]
-    urllib.request.install_opener(opener)
-
-    ## 드라마
-    #### ------------ 무한 로딩 ------------ ####
-    driver.get(first_video_path)
-    time.sleep(10)
+    ## Netflix
+    driver = request_netflix(first_video_path)
 
     ## 총 에피소드
     # while ep_cnt < total_episode:
@@ -145,7 +156,10 @@ def change_cc(first_video_path, total_episode):
             
             langs = driver.find_elements(By.TAG_NAME, 'li')
             for lang in langs:
-                if lang.text == "한국어":
+                ## -----------lang 변경----------- ##
+                # print(lang.text)
+                if lang.text == "중국어(간체)" or lang.text == "중국어(간체) [CC]":
+                # if lang.text == "중국어(번체)":
                     lang.click()
                     time.sleep(4)
                     break
@@ -172,11 +186,11 @@ def change_cc(first_video_path, total_episode):
                     
         print("----------------- 추출 종료 -----------------")
 
-## en-ko / ko-en
-trans_lang(r"https://www.netflix.com/watch/80988020?trackId=255824129",6)
+## en-ko / ko-en / ja-ko
+trans_lang(r"https://www.netflix.com/watch/81312811?trackId=255824129",8)
 
-## ko-ja / ko-zh = 자막 언어 변경 후 적용
-# change_cc(r"https://www.netflix.com/watch/81111433?trackId=255824129",6)
+# ko-ja / ko-zh / zh-ko = 자막 언어 변경 후 적용
+# change_cc(r"https://www.netflix.com/watch/81573832?trackId=255824129",20)
 
 
 
@@ -223,10 +237,35 @@ def scrap_mulitprocessing(search_name, debug_port):
     driver.find_element(By.XPATH, '//*[@id="title-card-0-0"]/div[1]/a/div[1]/img').click()
     driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[2]/div/div[1]/div[4]/div').click()
 
+'''
+series = [
+    "터닝포인트",
+    "크라임씬",
+    "휴먼",
+    "킬러샐리",
+    "타일러헨리",
+    "헤드스페이스숙면이필요할때",
+    "마음을바꾸는방법",
+    "F1본능의질주"
+]
 
-# if __name__ == "__main__":
-#     s_time = time.time()
-#     cpu_cnt = os.cpu_count() ## 최대 12개
-#     pool = Pool(processes=cpu_cnt)
-#     pool.starmap(scrap_mulitprocessing, zip(series, debug_ports))
-#     print("--- %s seconds ---" % (time.time() - s_time))
+debug_ports = [
+    9222,
+    9223,
+    9224,
+    9225,
+    9226,
+    9227,
+    9228,
+    9229
+    
+]
+
+
+if __name__ == "__main__":
+    s_time = time.time()
+    cpu_cnt = os.cpu_count() ## 최대 12개
+    pool = Pool(processes=cpu_cnt)
+    pool.starmap(scrap_mulitprocessing, zip(series, debug_ports))
+    print("--- %s seconds ---" % (time.time() - s_time))
+'''
