18171038f3b51877db580a71d3e4d3e10fc47a7b
YOUNG HUN KIM <83389640+kimmy-git@users.noreply.github.com>
2023-05-15 16:49:04 +0900

    Update and rename test.py to netflix_script.py

--- scraping_py/netflix/netflix_script.py
+++ scraping_py/netflix/netflix_script.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+# DeprecationWarning: executable_path has been deprecated, please pass in a Service object
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import time
 from makedir import make_dir
@@ -27,6 +30,9 @@ def request_netflix(first_video_path):
     # options.add_argument("--load-extension={}".format(r'C:\Users\yhunkim\AppData\Local\Google\Chrome\User Data\Default\Extensions\hoombieeljmmljlkjmnheibnpciblicm\5.0.0_0'))
     # options.add_extension(r"hoombieeljmmljlkjmnheibnpciblicm.crx")
     driver = webdriver.Chrome(executable_path= r"C:\Users\yhunkim\Desktop\capture\chromedriver_win32 (1)\chromedriver.exe", options=options)
+
+    # service = selenium 드라이버를 chrome버전에 맞춰 다운로드하고 경로를 찾을 필요 없어
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) 
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-Agent', 'Mozila/5.0')]
     urllib.request.install_opener(opener)
@@ -39,12 +45,18 @@ def request_netflix(first_video_path):
 
 def skip_next_btn(driver):
     ## ---------------- btn Xpath가 다를 수 있음 ---------------- ##
+    ## -- skip_btn(애니 / 영화,드라마 == 그때그때 다름) -- ##
+    # '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[3]/div[2]/button'
+    # '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[2]/div[2]/button'
+
     skip_btn = '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[2]/div[2]/button'
-    next_btn = '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[3]/div[2]/div/div/div[3]/div/div[3]/div[2]/button'
-    ## 영어, 일어
-    # //*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[3]/div[2]/div/div/div[3]/div/div[3]/div[2]/button
-    ## 중국어
-    # //*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[2]/div[2]/div/div/div[3]/div/div[3]/div[2]/button'
+    next_btn = '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[2]/div[2]/div/div/div[3]/div/div[3]/div[2]/button'
+
+    ### -- next_btn -- ##
+    ## 일어
+    # '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[3]/div[2]/div/div/div[3]/div/div[3]/div[2]/button'
+    ## 한국어, 영어, 중국어
+    # '//*[@id="appMountPoint"]/div/div/div[1]/div[8]/div[2]/div[2]/div[2]/div/div/div[3]/div/div[3]/div[2]/button'
 
     try:
         ## opening skip ##
@@ -152,21 +164,32 @@ def change_cc(first_video_path, total_episode):
             
             ## 언어 설정(= 원어가 한국어인 경우, [영어,일어,중국어])
             driver.find_element(By.XPATH, '//*[@id="select2-lln-NSL-dropdown-container"]').click()
-            time.sleep(1)
+            time.sleep(2)
             
             langs = driver.find_elements(By.TAG_NAME, 'li')
+            
             for lang in langs:
                 ## -----------lang 변경----------- ##
                 # print(lang.text)
-                if lang.text == "중국어(간체)" or lang.text == "중국어(간체) [CC]":
-                # if lang.text == "중국어(번체)":
+
+                # "중국어(간체)", "중국어(간체) [CC]"
+                # "중국어(번체)", "중국어(번체) [CC]"
+                ## [CC]없는게 첫번째로 나옴
+                if lang.text == "중국어(간체)":
                     lang.click()
                     time.sleep(4)
                     break
+                
+                ## 없으면 [CC]
+                else:
+                    if lang.text == "중국어(간체) [CC]":
+                        lang.click()
+                        time.sleep(6)
+                        break
 
             ## 닫기 버튼
             driver.find_element(By.XPATH, '//*[@id="lln-options-modal"]/div/div[4]/div').click()
-            time.sleep(1)
+            time.sleep(4)
         
             ## 내보내기 아이콘 
             driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[1]/div[1]/div[5]').click()
@@ -187,11 +210,10 @@ def change_cc(first_video_path, total_episode):
         print("----------------- 추출 종료 -----------------")
 
 ## en-ko / ko-en / ja-ko
-trans_lang(r"https://www.netflix.com/watch/81312811?trackId=255824129",8)
+# trans_lang(r"https://www.netflix.com/watch/81237945?trackId=255824129",10)
 
 # ko-ja / ko-zh / zh-ko = 자막 언어 변경 후 적용
-# change_cc(r"https://www.netflix.com/watch/81573832?trackId=255824129",20)
-
+change_cc(r"https://www.netflix.com/watch/81004129?trackId=200257858",15)
 
 
 ## 도중에 멈췄을때/해당 영상 스크립트 다운받아졌으면
@@ -199,73 +221,5 @@ trans_lang(r"https://www.netflix.com/watch/81312811?trackId=255824129",8)
 # 2. cp_ent = load된 파일 번호
 # 3. 다운로드 파일 이동(= 구현 X)
 # -------- 다 추출하고 cp_ent = 0으로 초기화 --------
-# -------- 영어,일어,중국어(간체)의 경우 처음에 설정 바궈서 진행 == 바꾸면 url 바뀜?? 다시 1화 영상 url 적용 --------
-
-def scrap_mulitprocessing(search_name, debug_port):
-    options = webdriver.ChromeOptions()
-    # prefs = {'download.default_directory' : r'C:\Users\yhunkim\Desktop\netflix_translation6\ko-en\나홀로그대'}
-    # options.add_experimental_option('prefs', prefs)
-    
-    ## GPU 가속 활성
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-setuid-sandbox')
-    options.add_argument('--no-sandbox')
-
-    # options.add_experimental_option('debuggerAddress', "127.0.0.1:9222")
-    options.add_argument(f"--remote-debugging-port={debug_port}")
-    ## extentions 적용 => 안됨 why?? / => 새로 extentions 설치로 진행
-    # options.add_argument("--load-extension={}".format(r'C:\Users\yhunkim\AppData\Local\Google\Chrome\User Data\Default\Extensions\hoombieeljmmljlkjmnheibnpciblicm\5.0.0_0'))
-    # options.add_extension(r"hoombieeljmmljlkjmnheibnpciblicm.crx")
-    driver = webdriver.Chrome(executable_path= r"C:\Users\yhunkim\Desktop\capture\chromedriver_win32 (1)\chromedriver.exe", options=options)
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-Agent', 'Mozila/5.0')]
-    urllib.request.install_opener(opener)
-
-    ## 드라마
-    #### ------------ 무한 로딩 ------------ ####
-    driver.get(r'https://www.netflix.com/browse')
-    driver.implicitly_wait(10)
-
-    driver.find_element(By.CSS_SELECTOR, '#appMountPoint > div > div > div:nth-child(1) > div.bd.dark-background > div.pinning-header > div > div > div > div:nth-child(1) > div > button').click()
-    driver.find_element(By.ID, 'searchInput').send_keys(search_name) ## Keys.RETURN 필요 없음
-
-    ## 검색 후 로딩
-    time.sleep(1)
-
-    ## 첫번째 영상 클릭 후 해당 영상 재생
-    driver.find_element(By.XPATH, '//*[@id="title-card-0-0"]/div[1]/a/div[1]/img').click()
-    driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[2]/div/div[1]/div[4]/div').click()
-
-'''
-series = [
-    "터닝포인트",
-    "크라임씬",
-    "휴먼",
-    "킬러샐리",
-    "타일러헨리",
-    "헤드스페이스숙면이필요할때",
-    "마음을바꾸는방법",
-    "F1본능의질주"
-]
-
-debug_ports = [
-    9222,
-    9223,
-    9224,
-    9225,
-    9226,
-    9227,
-    9228,
-    9229
-    
-]
-
-
-if __name__ == "__main__":
-    s_time = time.time()
-    cpu_cnt = os.cpu_count() ## 최대 12개
-    pool = Pool(processes=cpu_cnt)
-    pool.starmap(scrap_mulitprocessing, zip(series, debug_ports))
-    print("--- %s seconds ---" % (time.time() - s_time))
-'''
+# -------- 영어,일어,중국어(간체)의 경우 처음에 설정 바궈서 진행 --------
+# ----- 일본어 / 영어,중국어 다음 버튼 위치가 다름(= Xpath, 숫자 변경)
